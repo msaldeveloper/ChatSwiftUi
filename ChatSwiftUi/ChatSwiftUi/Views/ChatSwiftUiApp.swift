bacacfebce75c8fa9fa21fa85d7c58db8cc1cade
msaldaopen <mario.saldana@geovoy.com>
2025-05-27 11:58:05 -0600

    add firestore config

--- ChatSwiftUi/ChatSwiftUi/Views/ChatSwiftUiApp.swift
+++ ChatSwiftUi/ChatSwiftUi/Views/ChatSwiftUiApp.swift
@@ -10,12 +10,19 @@ import FirebaseCore
 import FirebaseAppCheck
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
 
-    return true
-  }
+        #if DEBUG
+        // Activa App Check con proveedor de Debug
+        AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
+        #endif
+
+        return true
+    }
 }
 
 @main
