e2c9207b00719f19b20fa6e8f4733abf471e8797
msaldaopen <mario.saldana@geovoy.com>
2025-05-27 08:08:31 -0600

    setting project

---
+++ ChatSwiftUi/ChatSwiftUi/ChatSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSwiftUiApp.swift
+//  ChatSwiftUi
+//
+//  Created by Mario Saldana on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatSwiftUi/ChatSwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatSwiftUi
+//
+//  Created by Mario Saldana on 27/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
